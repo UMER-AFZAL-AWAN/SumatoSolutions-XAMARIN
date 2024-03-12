7aea5fb95dcf6f6c84851bdbfe47cd42f76ee383
Umer <Awan.umer.m@gmail.com>
2024-03-12 08:02:56 +0500

    Creation Of Main Project

---
+++ MainProjectToImplementAllLearntTopics/MainProj/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MainProj
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ MainProjectToImplementAllLearntTopics/MainProj/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MainProj
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
